10a0ab5f522be4d8d2d23f6e52d48ca0ddf616dc
Vaidy Shanmugam <vaidy.sds@gmail.com>
2023-12-11 18:47:55 -0600

    new endpoints for store and item

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,13 +9,35 @@ stores = [
             {
                 'name': 'chair',
                 'price': 15.99
+            },
+            {
+                'name': 'desk',
+                'price': 200
             }
         ]
     }
 ]
 
 
-# get stores
-@app.get("/stores")
+@app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {'stores': stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "store not found"}, 404
