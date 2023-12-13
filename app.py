4f3f87cf0233eac994c09d7f6ed73b8f40212980
Vaidy Shanmugam <vaidy.sds@gmail.com>
2023-12-13 06:19:03 -0600

    updated models

--- app.py
+++ app.py
@@ -1,65 +1,65 @@
 from flask import Flask, request
+from db import stores, items
+import uuid
 
 app = Flask(__name__)
-
-stores = [
-    {
-        'name': 'My Wonderful Store',
-        'items': [
-            {
-                'name': 'chair',
-                'price': 15.99
-            },
-            {
-                'name': 'desk',
-                'price': 200
-            }
-        ]
-    }
-]
-
-
-@app.get("/store")
-def get_stores():
-    return {'stores': stores}
-
-
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "store not found"}, 404
-
-
-@app.get("/store/<string:name>/item")
-def get_items_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "store not found"}, 404
 
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    if new_store["name"] not in [store["name"] for store in stores]:
-        stores.append(new_store)
-        return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
+    return new_store, 201
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "store not found"}, 404
     else:
-        return {"message": "store already exists"}, 400
+        item_id = uuid.uuid4().hex
+        new_item = {**item_data, "id": item_id}
+        items[item_id] = new_item
+        return new_item, 201
 
 
-@app.post("/store/<string:name>/item")
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            if new_item["name"] not in [item["name"] for item in store["items"]]:
-                store["items"].append(new_item)
-                return new_item, 201
-            else:
-                return {"message": "item already exists"}, 400
-    return {"message": "store not found"}, 404
+# get all stores
+@app.get("/store")
+def get_stores():
+    return {'stores': list(stores.values())}
+
+
+# get a store by id
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "store not found"}, 404
+
+
+# get all items
+@app.get("/item")
+def get_items():
+    return {'items': list(items.values())}
+
+
+# get all items in a store
+@app.get("/store/<string:store_id>/item")
+def get_items_in_store(store_id):
+    try:
+        return {"items": [item for item in items.values() if item["store_id"] == store_id]}
+    except KeyError:
+        return {"message": "store not found"}, 404
+
+
+# get an item by id
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "item not found"}, 404
